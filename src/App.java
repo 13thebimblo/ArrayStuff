024db7073b746a50d65844e7bf89155cd4f427b0
13thebimblo <whazeb07@gmail.com>
2022-12-01 09:46:08 -0600

    Arrays

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int a[] = {1,2,3,4};
+        String hollywoodDudes[] = {"Tom Hanks", "John Williams", "Denzel Washington", "Woody Harrelson", "Michael Giacchino", "Mr. Trainor", "Orlando Bloom", "Jason Momoa"};
+    
+        printArray(hollywoodDudes);
+        
+    
     }
+
+    public static String[] printArray(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            //hollywoodDudes[i] = hollywoodDudes[i].toLowerCase();
+            System.out.println(array[i]);
+        }
+        return array;
+    }
+
 }
