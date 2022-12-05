dc13bf35668b0893da2b0bb565668ba983f46343
13thebimblo <whazeb07@gmail.com>
2022-12-05 09:47:06 -0600

    Update

--- src/App.java
+++ src/App.java
@@ -1,12 +1,39 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) throws Exception {
 
-        int a[] = {1,2,3,4};
-        String hollywoodDudes[] = {"Tom Hanks", "John Williams", "Denzel Washington", "Woody Harrelson", "Michael Giacchino", "Mr. Trainor", "Orlando Bloom", "Jason Momoa"};
+    //     String hollywoodDudes[] = {"Tom Hanks", "John Williams", "Denzel Washington", "Woody Harrelson", "Michael Giacchino", "Mr. Trainor", "Orlando Bloom", "Jason Momoa"};
     
-        printArray(hollywoodDudes);
+    
+    //     // printArray(hollywoodDudes);
+    //     Scanner in = new Scanner(System.in);
+    //     String s = in.nextLine();
+    //     String a[] = s.split(",");
+       
+    //     int intArray[] = new int[a.length];
+
+    //     for (int i = 0; i <intArray.length; i++) {
+    //         intArray[i] = Integer.parseInt(a[i]);
+    //     }
+    //     printArray(intArray);
         
+    int a[][] = {{1,2,3},{4,5,6}};
+    String employees[][] = {{"William","Brady","Hand","$40K"},
+                            {"Kimory","Stanley","Janitor","$200K"}};
+
+    printArray(employees);
+
+    for (int i = 0; i < a.length; i++) {
+        for (int j = 0; j < a[0].length; j++) {
+            a[i][j] *= a [i][j];
+
+        }
+    }
+
+    printArray(a);
+
     
     }
 
@@ -17,5 +44,33 @@ public class App {
         }
         return array;
     }
+   
+    public static int[] printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            //hollywoodDudes[i] = hollywoodDudes[i].toLowerCase();
+            System.out.println(array[i]);
+        }
+        return array;
+    }
 
+    public static int[][] printArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                System.out.print(array[i][j] + ", ");
+            }
+            System.out.print("\n");
+        }
+        return array;
+    }
+    
+    public static String[][] printArray(String[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                System.out.print(array[i][j] + ", ");
+            }
+            System.out.println();
+        }
+        return array;
+    }
+    
 }
